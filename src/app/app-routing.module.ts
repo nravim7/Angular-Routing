18abeb9501b1a5a14f627b52163ac99d12e9b31d
Ravi N <your.email@example.com>
2024-02-04 22:33:53 +0530

    Angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path:'home', component: HomeComponent },
-  {path:'settings', component: SettingsComponent}
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    children: [
+      
+    ]},
+  {}
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
